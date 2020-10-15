29004fb0035e798035ab62b59a4a2457037de291
ebussiere <eddy@embtech.ca>
2020-10-15 02:56:09 +0000

    Add SQL injection sanitizer

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,20 +6,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const inputArgs = function() {
-  return process.argv.slice(2);
-};
 
-pool.query(`
+const cohort = process.argv[2] || 'JUL02';
+const values = [cohort];
+
+const queryString = `
 SELECT  teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
 FROM teachers 
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id =students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name='${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name=$1 
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(item => {
       console.log(`${item.cohort}: ${item.teacher}`);
